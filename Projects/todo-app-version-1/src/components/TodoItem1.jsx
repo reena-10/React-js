efb45cbe34d85252eb1a9453cb133442efbc7f20
reena-10 <reenamahto310@gmail.com>
2026-02-09 15:00:13 +0530

    Daily update

---
+++ Projects/todo-app-version-1/src/components/TodoItem1.jsx
@@ -0,0 +1,16 @@
+function TodoItem1() {
+  let todoName = "BuyMilk";
+  let todoDate = "4/02/2026";
+  return (
+    <div class="row kg-row">
+      <div class="col-6">{todoName}</div>
+      <div class="col-4">{todoDate}</div>
+      <div class="col-2">
+        <button type="button " class="btn btn-danger kg-button">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+export default TodoItem1;
---
+++ Projects/todo-app-version-1/src/components/TodoItem2.jsx
@@ -0,0 +1,14 @@
+function TodoItem2() {
+  return (
+    <div class="row kg-row">
+      <div class="col-6">Complete Syllabus</div>
+      <div class="col-4">09/02/2026</div>
+      <div class="col-2">
+        <button type="button " class="btn btn-danger kg-button">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+export default TodoItem2;
